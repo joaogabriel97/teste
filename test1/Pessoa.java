4146bd495033803e0d720cddf2bbac95570da941
joaoufms <gabriel_fla04@hotmail.com>
2018-05-31 14:44:15 -0400

    thfghgfhfgh

---
+++ test1/Pessoa.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pessoa;
+
+/**
+ *
+ * @author i5
+ */
+public class Pessoa {
+    
+    private String nome;
+    private String dtn;
+    private String sexo;
+    private String cpf;
+    //private Conta[] con;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the dtn
+     */
+    public String getDtn() {
+        return dtn;
+    }
+
+    /**
+     * @param dtn the dtn to set
+     */
+    public void setDtn(String dtn) {
+        this.dtn = dtn;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the con
+     */
+    /*public Conta[] getCon() {
+        return con;
+    }*/
+
+    /**
+     * @param con the con to set
+     */
+    /*public void setCon(Conta[] con) {
+        this.con = con;
+    }*/
+    
+    
+}
+
